7f2a6755e2a5ebbaa077079c4843429f36622bb1
Kowa <sarahgabriela899@gmail.com>
2025-05-10 22:08:20 -0300

    chat e suas engenhocas

--- pages/index.js
+++ pages/index.js
@@ -6,3 +6,18 @@ export default function Home() {
     </main>
   );
 }
+
+    <div>
+      <h1>Bem-vindo ao Via Sancta!</h1>
+      <nav>
+        <ul>
+          <li><Link href="/santos"><a>Santos</a></Link></li>
+          <li><Link href="/oracoes"><a>Orações</a></Link></li>
+          <li><Link href="/milagres-eucaristicos"><a>Milagres Eucarísticos</a></Link></li>
+          <li><Link href="/livros"><a>Biblioteca</a></Link></li>
+          <li><Link href="/sobre"><a>Sobre</a></Link></li>
+          <li><Link href="/contato"><a>Contato</a></Link></li>
+        </ul>
+      </nav>
+    </div>
+
